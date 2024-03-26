dc4704d2741561b5e5ff0b41a929267f1d91f942
yassinyussuf <yassinyussuf414@gmail.com>
2024-03-26 12:26:39 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+function superbowlWin(record){
+    let wins = record.find((rcd) => rcd.result === "W");
+    if (wins){
+
+        return wins.year 
+    }
+    
+}
+superbowlWin(record)
